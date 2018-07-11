a40a52f953dc1f72a20f8d7b56b30b6cf1e4948b
Murial Anindo <twentymurial@outlook.com>
2018-07-11 12:35:25 -0400

    changes

---
+++ project3/src/App.js
@@ -0,0 +1,53 @@
+import React,{ Component } from 'react';
+import movieList from './components/movieList';
+import movieItem from './components/movieItem';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  deleteKey=(event)=>{
+    event.preventDefault(); 
+    this.setState({
+      
+    })
+  }
+ 
+  render() {
+    return (
+         <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Netflix meets Amazon</h1>
+        </header>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <movieList items={this.state.items} />
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
