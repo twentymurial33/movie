e3978f01862348e45a9bd81a2a62a71675e51784
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-17 20:29:37 -0400

    deletesearchnav

--- project3/src/components/Navbar/Navbar.js
+++ project3/src/components/Navbar/Navbar.js
@@ -14,11 +14,6 @@ const Navbar = () => (
            <form class="col s12">
 
              <div class="row">
-               <div class="input-field col s6">
-               <i class="material-icons prefix cyan-text text-accent-2">search</i>
-                 <input id="search" type="text" class="validate" />
-                 <label for="search">Search</label>
-               </div>
              </div>
            </form>
           </div>
