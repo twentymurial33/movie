71ed4842bf82f892c0245c56fb57b6c6406c23d7
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-21 12:15:40 -0400

    heroku push

--- project3/server/server.js
+++ project3/server/server.js
@@ -6,6 +6,8 @@ var mongoose=require("mongoose");
 var port = 5000 || process.env.PORT
 var router = express.Router()
 
+
+
 /** set up routes {API Endpoints} */
 routes(router)
 
@@ -14,7 +16,7 @@ app.use(bodyParser.json())
 
 //app.use('/static',express.static(path.join(__dirname,'static')))
 app.use('/api', routes)
-var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"
+var url = process.env.MONGODB_URI || "mongodb://johnsonnic222:DCBadgers22!!@ds147361.mlab.com:47361/heroku_8493vb2d"
 try {
     mongoose.connect(url, {
         // useNewUrlParser: true 
