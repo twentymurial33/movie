8006e8eb1f46994d6abaaf2145f14d5a37e8d5f1
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-22 14:51:14 -0400

    herokuuu

--- project3/server.js
+++ project3/server.js
@@ -15,13 +15,38 @@ app.get('/api/movies', (req, res) => {
   
     res.json(customers);
   });
-var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"
-try {
-    mongoose.connect(url, {
-    })    
-} catch (error) {
+
+  var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"     
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+// var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"
+// try {
+//     mongoose.connect(url, {
+//     })    
+// } catch (error) {
     
-}
+// }
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
