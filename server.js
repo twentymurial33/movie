639cb94a7ef70fa4e283908bb02b78ff43c158fb
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-22 15:53:19 -0400

    testing

--- server.js
+++ server.js
@@ -19,35 +19,36 @@ app.get('/api/movies', (req, res) => {
   var mongodb = require('mongodb');
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+// var MongoClient = mongodb.MongoClient;
 
-// Connection URL. This is where your mongodb server is running.
+// // Connection URL. This is where your mongodb server is running.
 
-//(Focus on This Variable)
-var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"     
-//(Focus on This Variable)
+// //(Focus on This Variable)
+// var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"     
+// //(Focus on This Variable)
 
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
+// // Use connect method to connect to the Server
+//   MongoClient.connect(url, function (err, db) {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err);
+//   } else {
+//     console.log('Connection established to', url);
 
-    // do some work here with the database.
+//     // do some work here with the database.
 
-    //Close connection
-    db.close();
-  }
-});
-// var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"
-// try {
-//     mongoose.connect(url, {
-//     })    
-// } catch (error) {
-    
-// }
-
-// app.listen(port, () => {
-//     console.log(`Server started at port: ${port}`);
+//     //Close connection
+//     db.close();
+//   }
 // });
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/movie"
+try {
+    mongoose.connect(url, {
+      useNewUrlParser: true
+    })    
+} catch (error) {
+    
+}
+
+app.listen(port, () => {
+    console.log(`Server started at port: ${port}`);
+});
