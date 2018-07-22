f771f739da5ab8387971989d8a294abeb3bff8ed
Murial Anindo <twentymurial@outlook.com>
2018-07-21 22:42:13 -0400

    favorites page

--- project3/src/pages/Favorites/Favorites.js
+++ project3/src/pages/Favorites/Favorites.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import MovieCard from "../../components/MovieCard/MovieCard.js";
+import './style.css';
 
 const Favorites=()=>(
 <div className="container ">
              <div class="card">
              <div class="card-image">
              <div class="col-lg-20">
-             <h3>Movie Favorites</h3>
-          <a
-            rel="noreferrer noopener"
-            target="_blank "
-          />
-             
-             <div class="card">
-             <div class="card-image">
-             <div class="col-lg-20">
-             <p>View movies From Your Movie Selection!</p>
-             <div class="card-image">
-           </div>
+             <h4>Movie Favorites</h4>
          </div>
       </div>
 </div>
-</div>
-</div>
-  </div>
 </div>
 )
 
