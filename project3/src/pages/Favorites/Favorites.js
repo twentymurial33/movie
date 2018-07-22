d0e97716ad01b85386df4b511e337463260d1ebf
Murial Anindo <twentymurial@outlook.com>
2018-07-21 21:18:42 -0400

    changes to favorites

--- project3/src/pages/Favorites/Favorites.js
+++ project3/src/pages/Favorites/Favorites.js
@@ -3,11 +3,29 @@ import {Link} from 'react-router-dom';
 import MovieCard from "../../components/MovieCard/MovieCard.js";
 
 const Favorites=()=>(
-    <div>
-
-      <h3>Your Favorites</h3>
-
-    </div>
-);
+<div className="container ">
+             <div class="card">
+             <div class="card-image">
+             <div class="col-lg-20">
+             <h3>Movie Favorites</h3>
+          <a
+            rel="noreferrer noopener"
+            target="_blank "
+          />
+             
+             <div class="card">
+             <div class="card-image">
+             <div class="col-lg-20">
+             <p>View movies From Your Movie Selection!</p>
+             <div class="card-image">
+           </div>
+         </div>
+      </div>
+</div>
+</div>
+</div>
+  </div>
+</div>
+)
 
 export default Favorites;
