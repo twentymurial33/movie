0247f160cad943087a1d4924eeaffc219d1308e0
Nicole Johnson <johsnonbadgersnic222@gmail.com>
2018-07-24 19:48:28 -0400

    add

--- project3/server.js
+++ project3/server.js
@@ -53,7 +53,7 @@ app.post('/api/favorite', (req, res) => {
   })
 })
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
