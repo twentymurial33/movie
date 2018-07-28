8f7fd807c6375375fc442039d1b6f0b4e5ef3d33
Murial Anindo <twentymurial@outlook.com>
2018-07-28 07:40:17 -0400

    very last

--- project3/server.js
+++ project3/server.js
@@ -59,15 +59,15 @@ app.post('/api/favorite', (req, res) => {
   })
 })
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, "./build/index.html"));
+// });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("project3/build"));
- } else {
-  app.use(express.static("public"));
- }
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("project3/build"));
+//  } else {
+//   app.use(express.static("public"));
+//  }
 
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
